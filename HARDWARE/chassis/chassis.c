9e5fa7bce42ec55a578f9e04fe0e4e25c88f74d1
ZhaoWeizhi <zhaoweizhi.zwz@gmail.com>
2022-08-15 21:18:29 +0800

    校准陀螺仪，速度控制改为世界坐标系，调角度pid参数

--- HARDWARE/chassis/chassis.c
+++ HARDWARE/chassis/chassis.c
@@ -7,10 +7,16 @@ Chassis chassis;
 //转速单位：rpm
 void chassisSetState(float vx, float vy, float targetAngle)
 {
-	//把mm/s转换成rpm
-	chassis.translationSpeed_1 = (vx / 2 - vy * (1.732 / 2)) * 60 / (WHEEL_DIAMETER * 3.1415);
-	chassis.translationSpeed_2 = (vx / 2 + vy * (1.732 / 2)) * 60 / (WHEEL_DIAMETER * 3.1415);
-	chassis.translationSpeed_3 = -vx * 60 / (WHEEL_DIAMETER * 3.1415);
+	float body_vx, body_vy;
+	body_vx = vx * cos(-chassis.angle / 360 * (2 * 3.14159)) 
+						- vy * sin(-chassis.angle / 360 * (2 * 3.14159));
+	body_vy = vx * sin(-chassis.angle / 360 * (2 * 3.14159)) 
+						+ vy * cos(-chassis.angle / 360 * (2 * 3.14159));
+	
+	//把mm/s转换成rpm	
+	chassis.translationSpeed_1 = (body_vx / 2 - body_vy * (1.732 / 2)) * 60 / (WHEEL_DIAMETER * 3.1415);
+	chassis.translationSpeed_2 = (body_vx / 2 + body_vy * (1.732 / 2)) * 60 / (WHEEL_DIAMETER * 3.1415);
+	chassis.translationSpeed_3 = -body_vx * 60 / (WHEEL_DIAMETER * 3.1415);
 	
 	//                       |  一秒底盘转几圈   |    乘旋转一圈轮子转动距离    |乘60秒|除轮子周长
 	// = 一秒轮子滚多远 * 60 / 轮子周长
@@ -29,7 +35,6 @@ void chassisSetState(float vx, float vy, float targetAngle)
 
 void chassisAngleRing(void)
 {
-	//待检查可能有bug
 	while(chassis.targetAngle >= chassis.angle + chassis.numOfTurns * 360.0 + 180) chassis.targetAngle -= 360;
 	while(chassis.targetAngle < chassis.angle + chassis.numOfTurns * 360.0 - 180) chassis.targetAngle += 360;
 	chassis.rotatingSpeed = -pidOutput(&(chassis.AngleRing_pid), chassis.targetAngle, chassis.angle + chassis.numOfTurns * 360.0);
@@ -45,7 +50,7 @@ void chassisAngleRing(void)
 
 void chassisInit(void)
 {
-	pidInit(&(chassis.AngleRing_pid), 0.5, 0, 0);
+	pidInit(&(chassis.AngleRing_pid), 0.45, 0.00012, 10.0);
 	chassis.world_x = 0;
 	chassis.world_y = 0;
 	chassis.angle = 0;
@@ -76,7 +81,7 @@ void USART2_IRQHandler(void)
 			if(gyroDataFrame.msgID == UP_ATTITUDE)
 			{
 				//chassis.angle = -attitude.yaw;
-				chassis.angle = -attitude.yaw - chassis.angleBias;
+				chassis.angle = attitude.yaw - chassis.angleBias;
 				if(chassis.lastAngle == 361) chassis.lastAngle = chassis.angle;
 				if(chassis.angle >= 90 && chassis.lastAngle <= -90)
 				{
--- USER/main.c
+++ USER/main.c
@@ -3,6 +3,7 @@
 #include "grayScaleSensor.h"
 #include "gyro.h"
 #include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "uart.h"
 #include "can.h"
@@ -16,6 +17,7 @@ int main(void)
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
 	u8 res;
 	char command[100];
+	char buff[100];
 	u8 commandCnt = 0;
 	u8 commandFinish = 0;
 	can_init(CAN_SJW_1tq, CAN_BS2_2tq, CAN_BS1_3tq, 6, CAN_Mode_Normal);
@@ -36,10 +38,17 @@ int main(void)
 	int speed = 0;
 	float angle = 0;
 	
-	chassis.angleBias = -attitude.yaw;
-	chassis.angle = 0;
-	chassis.lastAngle = 361;
-	chassis.numOfTurns = 0;
+	
+	
+	u16 i;
+	
+	for(i = 0; i < 10000; i++)
+	{
+		chassis.angleBias = attitude.yaw;
+		chassis.angle = 0;
+		chassis.lastAngle = 361;
+		chassis.numOfTurns = 0;
+	}
 	
 	while(1)
 	{
@@ -71,23 +80,23 @@ int main(void)
 		{
 			if(strcmp(command, "W") == 0)
 			{
-				chassisSetState(0, speed, 0);
+				chassisSetState(0, speed, angle);
 			}
 			else if(strcmp(command, "S") == 0)
 			{
-				chassisSetState(0, -speed, 0);
+				chassisSetState(0, -speed, angle);
 			}
 			else if(strcmp(command, "A") == 0)
 			{
-				chassisSetState(-speed, 0, 0);
+				chassisSetState(-speed, 0, angle);
 			}
 			else if(strcmp(command, "D") == 0)
 			{
-				chassisSetState(speed, 0, 0);
+				chassisSetState(speed, 0, angle);
 			}
 			else if(strcmp(command, "reset") == 0)
 			{
-				chassis.angleBias = -attitude.yaw;
+				chassis.angleBias = attitude.yaw;
 				chassis.angle = 0;
 				chassis.lastAngle = 361;
 				chassis.numOfTurns = 0;
@@ -96,6 +105,11 @@ int main(void)
 			{
 				speed = atoi(command + 5);
 			}
+			else if(strcmp(command, "angle") == 0)
+			{
+				sprintf(buff, "%f\n", chassis.angle + chassis.numOfTurns * 360);
+				uart4_send(buff);
+			} 
 			else if(strstr(command, "angle") != NULL)
 			{
 				angle = atof(command + 5);
@@ -103,9 +117,7 @@ int main(void)
 			}
 			else
 			{
-				motorSetTargetRpm(&motor[1], 0);
-				motorSetTargetRpm(&motor[2], 0);
-				motorSetTargetRpm(&motor[0], 0);
+				chassisSetState(0, 0, angle);
 			}
 			uart4_send(command);
 			uart4_send("\n");
